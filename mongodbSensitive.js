590546a378b62fe498b905e118e92c31f4a10af5
Alex Aymkin <aymkin@gmail.com>
2020-02-01 12:58:37 +0200

    169 connecting to mongo

---
+++ mongodbSensitive.js
@@ -0,0 +1,6 @@
+const mongodbSensitive = {
+    uri:
+        'mongodbSensitive+srv://admin:myPass14@cluster0-hs8sk.mongodbSensitive.net/test?retryWrites=true&w=majority',
+}
+
+module.exports = mongodbSensitive
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const mongodb = require('../mongodbSensitive')
 
 const app = express()
+
+const mongoUri = mongodb.uri
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance...')
+})
+mongoose.connection.on('error', err => {
+    console.log('Error connecting to mono: ' + err)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello express')
