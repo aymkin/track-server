9b2e13bb33b0be05b00c8e3d8a0868023faf2a48
Alex Aymkin <aymkin@gmail.com>
2020-02-01 11:55:18 +0200

    install prettier + esling + and make husky run prettier on pre-commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello express')
-});
+})
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
-});
+})
