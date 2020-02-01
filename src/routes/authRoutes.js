cad12c6763d40114076db1c910824b01a146953d
Alex Aymkin <aymkin@gmail.com>
2020-02-01 17:10:33 +0200

    176 error handling if email in use or did not provide empty body request

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,15 @@ const router = express.Router()
 
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body
-  const user = new User({ email, password })
-  await user.save()
 
-  res.send('You made a POST request')
+  try {
+    const user = new User({ email, password })
+    await user.save()
+
+    res.send('You made a POST request')
+  } catch (err) {
+    return res.status(422).send(err.message)
+  }
 })
 
 module.exports = router
