a529d002c3ac458d9109e224df832b4b816d02ca
Alex Aymkin <aymkin@gmail.com>
2020-02-01 13:05:56 +0200

    170 make nodemon automatic restart the server

--- mongodbSensitive.js
+++
@@ -1,6 +0,0 @@
-const mongodbSensitive = {
-    uri:
-        'mongodbSensitive+srv://admin:myPass14@cluster0-hs8sk.mongodbSensitive.net/test?retryWrites=true&w=majority',
-}
-
-module.exports = mongodbSensitive
